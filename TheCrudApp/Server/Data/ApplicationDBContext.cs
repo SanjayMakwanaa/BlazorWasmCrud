a4c697f6895092fae4086c1e80adc389b49ea995
SanjayMakwanaa <sanjaymakwana1183@gmail.com>
2023-04-19 18:06:10 +0530

    git added crud app using blazor WebAssembaly

---
+++ TheCrudApp/Server/Data/ApplicationDBContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TheCrudApp.Shared.Models;
+
+namespace TheCrudApp.Server.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+        }
+        public DbSet<Developer> Developers { get; set; }
+    }
+}
